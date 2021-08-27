92dd1037665cd6e6afa5226fe3fdcff74db7738d
grmarxer <>
2021-08-26 18:55:37 -0700

    Update Remove_services_in_bulk_from_IQ_script.sh

--- BIG-IQ_API/Remove_services_and_trust_for_BIG-IP/scripts/Remove_services_in_bulk_from_IQ_script.sh
+++ BIG-IQ_API/Remove_services_and_trust_for_BIG-IP/scripts/Remove_services_in_bulk_from_IQ_script.sh
@@ -35,9 +35,9 @@
 ###  This is the only section of the script to be modified by user            ###
 ###                                                                           ###
 #################################################################################
-username=admin  				# replace admin with your BIG-IQ username
-password=default				# replace default with your BIG-IQ password
-mgmtIP="[fd00:172:aa:bb::10]"	# Enter your BIG-IQ mgmt IP address, if using IPV6 be sure to include brackets inside the quotes.
+username=admin                  # replace admin with your BIG-IQ username
+password=default                # replace default with your BIG-IQ password
+mgmtIP="[fd00:172:aa:bb::10]"   # Enter your BIG-IQ mgmt IP address, if using IPV6 be sure to include brackets inside the quotes.
 pathToFile="/var/tmp/machineId_file.txt"  # replace with path to the Machine ID text file you created
 
 
